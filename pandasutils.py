588dad8b78b5590c84ee1f880be2b95b771b1c36
Domagoj Vukusic <33196607+domi65@users.noreply.github.com>
2019-02-04 11:12:27 +0100

    Add files via upload

---
+++ pandasutils.py
@@ -0,0 +1,22 @@
+# copy to your .ipynb notebook: from pandasutils import *
+# example: if you import some of functions, note no brackets after name, like here:
+#     from gc_utils import sigmoid, relu, dictionary_to_vector
+
+# Optional:
+# import numpy as np
+# import pandas as pd
+
+# df_basic_info() is a function in pandasutils module, that shows Basic Information about a dataframe:
+def df_basic_info(df):
+    print('shape (rows, columns)= ', df.shape) #(rows, columns)
+    print()
+    print('index=', df.index) #Describe index
+    print()
+    print('columns, Describe DataFrame columns=', df.columns) #Describe DataFrame columns
+    print()
+    print('info=', df.info()) #Info on DataFrame
+    print()
+    print('count, Number of non-NA values=', df.count()) #Number of non-NA values
+    print()
+    return
+    
